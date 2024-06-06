ed86d1a3423b4ff30b77064673365798be086329
Kunal Deshmukh <deshmukh1207@gmail.com>
2024-06-06 17:18:00 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/Docs-mini-react/",
+  base : "/docs-mini-react/",
 })
